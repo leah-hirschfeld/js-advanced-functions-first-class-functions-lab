c1b947d010179c7076d65713a0d0091038a9843d
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-04-06 12:48:08 -0700

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+/// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2)
+  }
+  const returnLastTwoDrivers= function(drivers) {
+    return drivers.slice(-2)
+  }
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+  const createFareMultiplier = function(num) {
+    return function(fare) {
+      return fare * num
+    }
+  }
+  const fareDoubler = createFareMultiplier(2)
+  const fareTripler = createFareMultiplier(3)
+  const selectDifferentDrivers = function(drivers, anyFunc) {
+    return anyFunc(drivers)
+  }
